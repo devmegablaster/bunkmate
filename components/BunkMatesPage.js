7c99b0fbe7d70bd91e91659a654d6dd9b258ce9d
MEGA-BLASTER <devmegablaster@gmail.com>
2022-04-04 20:57:04 +0530

    Fine Tuning

--- components/BunkMatesPage.js
+++ components/BunkMatesPage.js
@@ -95,7 +95,7 @@ function BunkMatesPage({ session, data }) {
       </h2>
       <p className="px-5 text-gray-600 md:px-20">
         {data.room}, {data.block}-Block,{' '}
-        {data.gender == 'G' ? 'Gents Hostel' : 'Ladies Hostel'}
+        {data.type == 'G' ? 'Gents Hostel' : 'Ladies Hostel'}
       </p>
       <p className="mb-5 px-5 text-gray-600 md:px-20">
         {data.bunkMates.length} Bunkmates Found!
