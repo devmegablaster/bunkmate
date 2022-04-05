263a7f11a04fa1d064586007c26791fc5973d53a
MEGA-BLASTER <devmegablaster@gmail.com>
2022-04-05 11:40:06 +0530

    Added Large screen size support

--- components/BunkMatesPage.js
+++ components/BunkMatesPage.js
@@ -100,7 +100,7 @@ function BunkMatesPage({ session, data }) {
       <p className="mb-5 px-5 text-gray-600 md:px-20">
         {data.bunkMates.length} Bunkmates Found!
       </p>
-      <div className="mx-auto mb-10 grid grid-cols-1 gap-4 pt-5 md:mx-0 md:grid-cols-2 md:px-28 lg:grid-cols-4">
+      <div className="mx-auto mb-10 grid grid-cols-1 gap-4 pt-5 md:mx-0 md:grid-cols-2 md:px-28 lg:grid-cols-4 2xl:grid-cols-5">
         {data.bunkMates.map((bunkMate) => {
           return (
             <div
